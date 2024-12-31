18cf8720f16903bdc509fbe06c86e924fc3af7de
ankushudapure24 <ankush.udapure@kaliba.in>
2024-12-31 16:18:13 +0530

    Added few components to pm project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
+import { useState } from 'react';
+// import NewProject from "./components/NewProject.jsx";
 import ProjectSidebar from "./components/ProjectsSidebar.jsx";
+import NoProject from "./components/NoProject.jsx";
+import NewProject from './components/NewProject.jsx';
 
 function App() {
+
+  const [projectsState, setProjectState] = useState({
+    selecetedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectState(prevState =>{
+      return{
+        ...prevState,
+        selecetedProjectId : null,
+      };
+    });
+  }
+
+  let content;
+
+  if(projectsState.selecetedProjectId === null) {
+    content = <NewProject />
+  }else if (projectsState.selecetedProjectId === undefined) {
+    content = <NoProject onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-    <ProjectSidebar/>
+      <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+      {/* <NoProject onStartAddProject={handleStartAddProject}/> */}
+      {/* <NewProject/> */}
+      {content}
     </main>
   );
 }
